ba438b3c84a1d20d0bc9b1082add5a29486e6bbf
MikeTomato <mike@costabellarealtygroup.com>
2019-06-03 10:49:12 -0400

    smart path first push barebones multi form w semantic ui

---
+++ src/components/Confirmation.jsx
@@ -0,0 +1,53 @@
+// Confirmation.jsx
+import React, { Component } from 'react';
+import { Button, List } from 'semantic-ui-react';
+
+class Confirmation extends Component{
+  saveAndContinue = (e) => {
+    e.preventDefault()
+    this.props.nextStep()
+  }
+
+  back = (e) => {
+    e.preventDefault()
+    this.props.prevStep()
+  }
+
+  render(){
+    const {values: {firstName, lastName, email, age, city, country }} = this.props;
+
+    return(
+      <div>
+        <h1 className="ui centered">Confirm your Details</h1>
+        <p>Click Confirm if the following details have been correctly entered</p>
+        <List>
+          <List.Item>
+            <List.Icon name='users'/>
+            <List.Content>First Name: {firstName}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='users' />
+            <List.Content>Last Name: {lastName}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='mail' />
+            <List.Content> <a href='mailto:blah@gmail.com'>{email}</a></List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='calendar' />
+            <List.Content>{age}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='marker' />
+            <List.Content>{city}, {country}</List.Content>
+          </List.Item>
+
+          <Button onClick={this.back}>Back</Button>
+          <Button onClick={this.saveAndContinue}>Confirm</Button>
+        </List>
+      </div>
+    )
+  }
+}
+
+export default Confirmation;
